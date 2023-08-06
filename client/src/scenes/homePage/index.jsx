0fad90bc8c715b90441aae8e9287e31213f6da1b
HamzaK702 <125295517+HamzaK702@users.noreply.github.com>
2023-08-06 16:28:55 +0500

    Added Upload image Modal with upload button

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,11 +1,35 @@
 import { useSelector } from "react-redux";
+import { useState } from "react";
 import Navbar from "../navbar";
-import { Box, Typography   } from "@mui/material";
-import UserImage from "../../components/UserImage";
+import { Box, Button, Typography   } from "@mui/material";
+import FlexBetween from "../../components/FlexBetween";
+import Dropzone from "react-dropzone";
+ import UserImage from "../../components/UserImage";
  import EditIcon from '@mui/icons-material/Edit';
+ import Modal from '@mui/material/Modal';
+ 
+
+
+
  //import AddCircleIcon from '@mui/icons-material/AddCircle';
 const HomePage = () => {
-   
+  
+
+  const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const [image, setImage] = useState("");
 const user = useSelector((state) => state.user);
 const picturePath = useSelector((state) => state.user.picturePath);
 const fullName =  `${user.firstName} ${user.lastName}`;
@@ -13,9 +37,9 @@ function handleEditname(){
   console.log("Yes edit name clicked")
 }
 
-function handleAddpicture(){
+function UploadImage(){
  
-  console.log("Yes Add picture clicked")
+  console.log("Yes upload picture clicked")
 }
   return (
     <>
@@ -30,10 +54,44 @@ function handleAddpicture(){
         color='#FFF'
       > 
         <Box sx={{display:'flex'}}>
-        <Box onClick={handleAddpicture}>
+        <Box onClick={handleOpen}>
         <UserImage  image={picturePath} size="100px"></UserImage> 
         </Box>
-        
+        {/* Fazoo kaam in coming */}
+        {/* <Button onClick={handleOpen}>Open modal</Button> */}
+      <Modal
+        keepMounted
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="keep-mounted-modal-title"
+        aria-describedby="keep-mounted-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="keep-mounted-modal-title" variant="h6" component="h2">
+            Upload a picture
+          </Typography>
+          
+          
+          <Dropzone 
+              acceptedFiles=".jpg,.jpeg,.png"
+              multiple={false} 
+              onDrop={acceptedFiles => setImage(acceptedFiles[0])}>
+            {({getRootProps, getInputProps}) => (
+              <section>
+                <Box sx={{ border: 1, borderColor: 'primary.main', my:1, p:1}}>
+                <div {...getRootProps()}>
+                  <input {...getInputProps()} />
+                 {!image ? (
+                      <p>Add Image Here</p>
+                    ) : ( <Typography>{image.name}</Typography> )}
+                </div>
+                </Box>
+              </section>
+            )}
+          </Dropzone>
+          <Button variant="contained" onClick={UploadImage}>Upload</Button>
+         </Box>
+      </Modal>
         {/* <AddCircleIcon onClick={handleAddpicture}
         sx={{
           mt: '80px',
